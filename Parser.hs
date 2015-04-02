def802a108d54d90c4779008f02b965420f65d67
Thomas Emile Bourgeat <thomas.bourgeate@ens.fr>
2015-04-03 00:04:34 +0200

    Seems to work, I need to test it more carefully

---
+++ Parser.hs
@@ -0,0 +1,94 @@
+{-# LANGUAGE TupleSections, TypeOperators, EmptyDataDecls, KindSignatures #-}
+
+--module Parser where
+
+import Control.Monad
+import Control.Exception (assert)
+import Control.Applicative hiding ((<|>), many)
+import Data.Char
+import Data.List
+import Data.Functor
+import qualified Data.Map as Map
+
+import Text.Parsec 
+import Text.Parsec.String
+import Text.Parsec.Expr
+import Text.Parsec.Token
+ 
+import AST
+
+netlistParser :: Parser Circuit 
+netlistParser = spaces *> netlist <* eof
+
+
+netlist = Pr <$> inputs <*> outputs 
+             <*> equations
+
+
+token1 t = t <* spaces
+token2 t = spaces *> t 
+punctuation = token1 . char
+punctuation2 = token2 . char
+
+identStartChar = letter <|> char '_'
+identChar = identStartChar <|> digit 
+
+eodecl = punctuation ';'
+
+keyword x = token1 . try $ string x *> notFollowedBy identChar
+
+
+ident = try $ do
+  foo <- token1 ( (:) <$> identStartChar <*> many identChar )
+  if foo `elem` [".input", ".output"]
+    then mzero
+    else return foo
+
+bigList header eltParser = keyword header
+				   *> many eltParser   --`sepBy` (punctuation ',')
+
+inputs = bigList ".input" ident <* eodecl
+outputs = bigList ".output" ident <* eodecl
+
+equations = many $ equation <* eodecl
+
+equation = do z <- ident
+              punctuation '='
+              e <- exprParse
+              return (z, e)
+
+
+exprParse = undefined
+---------------------------
+-- Parser for expressions 
+---------------------------
+
+
+ident2 = try $ do
+  foo <- token2 ( (:) <$> identStartChar <*> many identChar )
+  if foo `elem` [".input", ".output"]
+    then mzero
+    else return foo
+
+
+expr    = buildExpressionParser table term
+
+term    =  do { punctuation2 '(' ; x <- expr ; punctuation2 ')' ; return x } 
+	<|> (Earg <$> ident2)
+table   = [ [prefix "!" (Enot), prefix "~" (Enot) ]
+          , [binary "&" (Ebinop And) AssocLeft, binary " " (Ebinop And) AssocLeft]
+	  , [binary "+" (Ebinop Or) AssocLeft ]
+          ]
+        
+binary  name fun assoc = Infix (do{ token1 . try . string $ name; return fun }) assoc
+prefix  name fun       = Prefix (do{ token2 . try . string $ name; return fun })
+
+
+
+   
+main :: IO()
+main =do
+	putStrLn "start" 
+	case parse expr "" "!a  !ab" of
+		Left a -> putStrLn "fail"
+		Right b -> putStrLn . show $ b
