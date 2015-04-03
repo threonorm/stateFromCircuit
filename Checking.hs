600a68cf303349e79bcaf421e6e646bf474b1c3d
Thomas Emile Bourgeat <thomas.bourgeate@ens.fr>
2015-04-03 16:02:20 +0200

    Add intermediate variables done

--- Checking.hs
+++ Checking.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE TupleSections, TypeOperators, EmptyDataDecls, KindSignatures #-}
 
---module Parser where
+module Checking where
 
 import Control.Monad
 import Control.Exception (assert)
@@ -16,83 +16,29 @@ import Text.Parsec.Expr
 import Text.Parsec.Token
  
 import AST
+import Parser
 
-netlistParser :: Parser Circuit 
-netlistParser = spaces *> netlist <* spaces <* eof
-
-
-netlist = Pr <$> inputs <*> outputs 
-             <*> equations
-
-
-token1 t = t <* spaces
-token2 t = spaces *> t 
-punctuation = token1 . char
-punctuation2 = token2 . char
-
-identStartChar = letter <|> char '_'
-identChar = identStartChar <|> digit 
-
-eodecl = punctuation ';'
-
-keyword x = token1 . try $ string x *> notFollowedBy identChar
+addIntermediateVariables :: Circuit -> FullCircuit
+addIntermediateVariables circuit = 
+	Fc (c_inputs circuit )
+		(c_outputs circuit) 
+		(interm)
+		(c_eqs circuit)  
+	where 	
+		interm = ((nub . concat . map (extractVariables . snd) $ c_eqs circuit)
+			\\ (c_outputs circuit))
+			\\ (c_inputs circuit)
+		extractVariables (Earg a) = [a]
+		extractVariables (Enot a) = extractVariables a
+		extractVariables (Ebinop _ a b) = extractVariables a
+						++ extractVariables b
 
 
-ident = try $ do
-  foo <- token1 ( (:) <$> identStartChar <*> many identChar )
-  if foo `elem` [".inputs", ".outputs"]
-    then mzero
-    else return foo
+-- We need to check that every output is defined
+-- That nobody is defined two times
+-- That everyone that is used is defined or is an input 
+checkSanityFullCircuit = undefined
 
-bigList header eltParser = keyword header
-				   *> many eltParser   --`sepBy` (punctuation ',')
-
-inputs = bigList ".inputs" ident <* eodecl
-outputs = bigList ".outputs" ident <* eodecl
-
-equations = many $ equation <* eodecl
-
-equation = do z <- ident
-              punctuation '='
-              e <- expr
-              return (z, e)
-
-
----------------------------
--- Parser for expressions 
----------------------------
-
-
-ident2 = try $ do
-  foo <- token2 ( (:) <$> identStartChar <*> many identChar )
-  if foo `elem` [".inputs", ".outputs"]
-    then mzero
-    else return foo
-
-expr    = buildExpressionParser table term
-
--- The next function will change if I add new operator
--- I need the list of all the binaries operators 
-
-term    =  do { punctuation2 '(' ; x <- expr ; punctuation2 ')' ; return x } 
-	<|> (Earg <$> ident2)
-
-table   = [[prefix "!" (Enot), prefix "~" (Enot), postfix "'" (Enot)]
-	,[Infix (try $ do{ string $ " ";
-			 try . token2. string $ ""; 
-			lookAhead expr;
-			return (Ebinop And) }) AssocLeft]
-	,[binary "&" (Ebinop And) AssocLeft]
-	  , [binary "+" (Ebinop Or) AssocLeft ]]
-        
-binary  name fun assoc = Infix (try $ do{  try . token1 . token2 . string $ name; return fun }) assoc
-prefix  name fun       = Prefix (try $do{ try . token2 . string $ name; return fun })
-
-postfix  name fun       = Postfix (try $do{ try . token2 . string $ name; return fun })
-
-
-
-   
 main :: IO()
 main =do
 	putStrLn "start" 
@@ -100,4 +46,7 @@ main =do
 	result <-parseFromFile netlistParser myLine
 	case result  of
 		Left a -> putStrLn "fail"
-		Right b -> putStrLn . show $ b
+		Right b -> putStrLn . show . addIntermediateVariables $ b
+
+
+  
