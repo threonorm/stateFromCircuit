83c485fb2be07e9d130d23e58e70ecf9ccf39676
Thomas Emile Bourgeat <thomas.bourgeate@ens.fr>
2015-04-03 15:06:22 +0200

    Small cleaning.

--- Parser.hs
+++ Parser.hs
@@ -17,38 +17,54 @@ import Text.Parsec.Token
  
 import AST
 
+------------------
+-- The language --
+------------------
+
+-- A - Main structure of a file
+
 netlistParser :: Parser Circuit 
 netlistParser = spaces *> netlist <* spaces <* eof
 
+netlist = Pr <$> inputs <*> outputs <*> equations
 
-netlist = Pr <$> inputs <*> outputs 
-             <*> equations
+-- I. Definitions
 
-
-token1 t = t <* spaces
-token2 t = spaces *> t 
-punctuation = token1 . char
-punctuation2 = token2 . char
-
+	-- a) Identifiers
 identStartChar = letter <|> char '_'
 identChar = identStartChar <|> digit 
 
-eodecl = punctuation ';'
+-- Be careful with keywords which are not identifiers
+-- Because of the whitespaces are significatives, I need
+-- two alternatives versions, the first one eat the right-trailing
+-- whitespaces, the second one eat the left-trailing whitespaces
+-- This is a painful complication
 
-keyword x = token1 . try $ string x *> notFollowedBy identChar
-
-
+ 
 ident = try $ do
   foo <- token1 ( (:) <$> identStartChar <*> many identChar )
   if foo `elem` [".inputs", ".outputs"]
     then mzero
     else return foo
 
-bigList header eltParser = keyword header
-				   *> many eltParser   --`sepBy` (punctuation ',')
+ident2 = try $ do
+  foo <- token2 ( (:) <$> identStartChar <*> many identChar )
+  if foo `elem` [".inputs", ".outputs"]
+    then mzero
+    else return foo
+
+
+	-- b) End of line
+eodecl = punctuation ';'
+
+	-- c) Comments TODO
+
+-- II. Headers
 
 inputs = bigList ".inputs" ident <* eodecl
 outputs = bigList ".outputs" ident <* eodecl
+
+-- III. Equations
 
 equations = many $ equation <* eodecl
 
@@ -57,41 +73,44 @@ equation = do z <- ident
               e <- expr
               return (z, e)
 
-
----------------------------
--- Parser for expressions 
----------------------------
-
-
-ident2 = try $ do
-  foo <- token2 ( (:) <$> identStartChar <*> many identChar )
-  if foo `elem` [".inputs", ".outputs"]
-    then mzero
-    else return foo
+-- IV. Expressions 
 
 expr    = buildExpressionParser table term
 
--- The next function will change if I add new operator
--- I need the list of all the binaries operators 
+-- The next function will change if I add new operators
 
 term    =  do { punctuation2 '(' ; x <- expr ; punctuation2 ')' ; return x } 
-	<|> (Earg <$> ident2)
+	<|> Earg <$> ident2
 
-table   = [[prefix "!" (Enot), prefix "~" (Enot), postfix "'" (Enot)]
-	,[Infix (try $ do{ string $ " ";
-			 try . token2. string $ ""; 
-			lookAhead expr;
-			return (Ebinop And) }) AssocLeft]
-	,[binary "&" (Ebinop And) AssocLeft]
-	  , [binary "+" (Ebinop Or) AssocLeft ]]
+table   = 	[[prefix "!" (Enot), prefix "~" (Enot), postfix "'" (Enot)]
+		,[Infix (try $ do{ string $ " ";  -- " " mean AND in some contexts
+			 	try . token2 . string $ ""; 
+				lookAhead expr;
+				return (Ebinop And) })
+			 AssocLeft]
+		,[binary "&" (Ebinop And) AssocLeft]
+	  	,[binary "+" (Ebinop Or) AssocLeft]]
         
 binary  name fun assoc = Infix (try $ do{  try . token1 . token2 . string $ name; return fun }) assoc
 prefix  name fun       = Prefix (try $do{ try . token2 . string $ name; return fun })
-
 postfix  name fun       = Postfix (try $do{ try . token2 . string $ name; return fun })
 
+-- Useful parsing tools
 
+keyword x = token1 . try $ string x *> notFollowedBy identChar
 
+-- The fact that a whitespace can be a AND make it painful, we need to
+-- sanitize to the left or to the right depending on the context
+token1 t = t <* spaces
+token2 t = spaces *> t 
+
+punctuation = token1 . char
+punctuation2 = token2 . char
+
+bigList header eltParser = keyword header *> many eltParser  
+
+-- Main 
+-- Warning : This part of the code will disappear later
    
 main :: IO()
 main =do
@@ -101,3 +120,6 @@ main =do
 	case result  of
 		Left a -> putStrLn "fail"
 		Right b -> putStrLn . show $ b
+
+
+
