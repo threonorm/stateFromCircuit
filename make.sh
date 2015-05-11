a395089f82ea2d5a320fdcecfdc29ece203a0f8a
Thomas Emile Bourgeat <thomas.bourgeate@ens.fr>
2015-05-11 20:49:50 +0200

    Small change in names

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ghc -O2 --make -static -optc-static -optl-static Logic.hs -fvia-C -optl-pthread
+ghc -O2 --make -static -optc-static -optl-static Sat.hs -fvia-C -optl-pthread
 ghc -O2 --make -static -optc-static -optl-static Reconstruction.hs -fvia-C -optl-pthread
 ghc -O2 --make -static -optc-static -optl-static Visualization.hs -fvia-C -optl-pthread
 
--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./Logic $1 > $1.eqn
+./Sat $1 > $1.eqn
 ./minisat+ $1.eqn > $1.sol
 ./Reconstruction $1 $1.sol | dot -Tsvg > $1.after.svg
 ./Visualization $1 | dot -Tsvg > $1.before.svg
