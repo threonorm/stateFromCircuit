33a71b68b9e888639d0ba89aaafa3cc0a3ba6030
Thomas Emile Bourgeat <thomas.bourgeate@ens.fr>
2015-05-19 21:04:39 +0200

    NAND and not XOR

--- Logic.hs
+++ Logic.hs
@@ -251,7 +251,7 @@ propagateSignals g nbInputs = normalize . foldl (\acc noeud ->acc `FOL.and`
 				) tt $ nodes g   
 
 
-mutuallyExclusive s1 s2 = (e1 `FOL.or` e2) `FOL.and` (FOL.not (e1 `FOL.and` e2))
+mutuallyExclusive s1 s2 = (FOL.not (e1 `FOL.and` e2))
 		where 	e1 = atom "E" [ Var $ s1, Var $ s2 ] 
 			e2 = atom "E" [ Var $ s2, Var $ s1 ] 
 
