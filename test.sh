c256c11e0bedca1cda75d229d936880ca909c2b2
Thomas Emile Bourgeat <thomas.bourgeat@ens.fr>
2015-05-27 10:50:27 +0200

    script

--- test.sh
+++ test.sh
@@ -5,3 +5,15 @@
 ./Visualization $1 > $1.before.dot #| dot -Tsvg > $1.before.svg
 dot -Tsvg $1.after.dot > graphTest.svg
 gnome-open graphTest.svg
+
+
+petrify -hide .internal $j.csg | write_sg -o $j.hide.sg
+
+genet -k 3 $i.sg -o $i.genet
+
+genet2async  $i.genet $i.csg > $i.pn
+
+draw_astg -ip -nonames $i.pn -o $i.ps
+
+ps2pdf $i.ps $i.pdf
+  
