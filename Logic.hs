a2ee7364d1124c7290f2d95f0d4a1519a68d04c8
Thomas Emile Bourgeat <thomas.bourgeat@ens.fr>
2015-05-28 15:06:59 +0200

    some clean up

--- Logic.hs
+++ Logic.hs
@@ -32,6 +32,7 @@ import Data.Graph.Inductive.Example
 import Data.Graph.Inductive.PatriciaTree
 
 --Come from the lib
+
 nodeComp :: Eq b => LNode b -> LNode b -> Ordering
 nodeComp n@(v,_) n'@(w,_) | n == n'   = EQ
                           | v<w       = LT
@@ -50,6 +51,10 @@ slabEdges = sortBy edgeComp . labEdges
 
 included:: (Eq a,Eq b,Graph gr) => gr a b -> gr a b -> Bool
 included g g' = slabNodes g == slabNodes g' && intersect (slabEdges g) (slabEdges g') == slabEdges g
+
+
+
+--- Isomorphism --
 
 subIsomorphisms g1 g2 = foldl
 	(\acc f -> \l -> f l >>= acc)
@@ -74,73 +79,6 @@ subIsomorphisms g1 g2 = foldl
 				 (nub.fmap (\(x,y)-> (renameBy (l!!(x-1)) l,renameBy (l!!(y-1)) l,())) $ edges g1) :: Gr () ())  
 		extractEdges l (x,y,z) = if elem x l && elem y l then Just (x,y,z) else Nothing 
 
-type Isomorphism = Map.Map Term Term
-type Isomorphisms = [Isomorphism]
-
---satifyF :: Isomorphisms -> INF -> INF
-satifyF g iso clause n =
-	catMaybes . fmap (removeWrong n)$ foldl (\acc i->(++) acc . catMaybes $ fmap 
-					(\(IClause a b) -> 
-							case satifyCP g i b of
-								Nothing -> Nothing
-								Just l ->  Just $ IClause (fromMaybe [] $ satifyCN g i a) (l))
-					 $ clause )
-		[]	
-		iso 
-
-removeWrong n clause = case clause of --this will do nothing for the last output persistency 
-	IClause ((Atom _ (Var s1:Var s1':_)):(Atom _ (Var s2:Var s2':_)):[]) (_)  ->  
-			let	p1 = event s1 s1' in		
-			let	p2 = event s2 s2' in	
-			if (True) -- if (p1>=n || p2>= n )
-					then Just clause
-					else Nothing 
-	_ -> Just clause
-
-event a b = fromJust . findIndex (\(x,y)-> x /=y )$ a `zip` b
-  
-satifyCP g i l =simplify g. fmap (substitue g i)$ l
-
-satifyCN g i l= simplify g. fmap (substitue g i) $l
-
---substitue :: Isomorphism -> Atom () -> Atom ()
-substitue g i (Atom s l) = --adaptation for persistency for this existing quantifier
-	Atom s ((fmap (\x -> if x == FOL.Const "Skol8" [Var "x4",Var "x2", Var "x1"] then existRemover g (i Map.! (Var "x1")) (i Map.! (Var "x2")) (i Map.! (Var "x4")) else i Map.! x) $ take 2 l)++drop 2 l) 
-
-existRemover g s1 s2 s3 =
-	case s3 of
-		Var s -> Var $ take (transition ) s ++ (\x->if x=='0' then "1" else "0") (s!!transition)  ++ drop (transition+1) s   
-		_ -> undefined
-	where 	transition =   fromJust . findIndex (\(x,y)-> x /=y ) $ (show s1) `zip` (show s2) 
-
-
-
---simplify :: [ Atom () ] -> [ Atom () ]
---This is not ocrrect because of the Eq proposition TODO
-simplify g x = foldl 
-		(\acc (Atom s l) -> case acc of
-					Nothing -> Nothing
-					Just j -> if s=="Eq" 
-						then	if l!!0 == l!!1 
-								then Nothing
-								else Just j
-			 			else if isIn l g 
-							then Just $ (Atom s l):j
-							else Just j) 
-		(Just [])
-		$ x	
-	
-
-isIn l g = case lookup (l!!0) $ noeuds of
-	Nothing -> False
-	Just firstVertice -> case lookup (l!!1) $ noeuds of
-		Nothing -> False
-		Just secondVertice -> elem secondVertice $ suc g firstVertice 
-	where 	noeuds = catMaybes . fmap (\(x,y)-> if (Var x) `elem` l 
-			then Just (Var x,y)
-			else Nothing )
-			$ fmap swap dic
-		dic = labNodes g
 
 allIsomorphisms formula stateGraph = 
 	 [isomorphisms pattern | pattern <- patterns]		
@@ -151,14 +89,14 @@ allIsomorphisms formula stateGraph =
 				 $ subIsomorphisms pattern stateGraph []
 		patterns = extractPattern formula 
 
-printSatFormulas [] stateGraph n = []
-printSatFormulas normalized@(t:q) stateGraph n = 
-	satifyF stateGraph (concat $ allIsomorphisms normalized stateGraph) normalized n
-	++ ( printSatFormulas q stateGraph n )
 
-intF [] = 0
-intF (t:q) = if t then 2* intF q +1 else 2 * intF q 
 
+
+type Isomorphism = Map.Map Term Term
+type Isomorphisms = [Isomorphism]
+
+---- INDEPENDENT of the formula
+ 
 convertGraph :: SG.StateGraph -> Gr String String
 convertGraph sg = --It is not fully built by lazyness,
 	mkGraph v e  --BIG IMPROVEMENT HERE TODO
@@ -203,7 +141,151 @@ extractPattern arg1@((IClause a q):l) = [inductivelyBuild a] --addVertex q (nub 
 				else withNode
 
 
---USELESS
+simplify g x = foldl 
+		(\acc (Atom s l) -> case acc of
+					Nothing -> Nothing
+					Just j -> if s=="Eq" 
+						then	if l!!0 == l!!1 
+								then Nothing
+								else Just j
+			 			else if isIn l g 
+							then Just $ (Atom s l):j
+							else Just j) 
+		(Just [])
+		$ x	
+	
+
+isIn l g = case lookup (l!!0) $ noeuds of
+	Nothing -> False
+	Just firstVertice -> case lookup (l!!1) $ noeuds of
+		Nothing -> False
+		Just secondVertice -> elem secondVertice $ suc g firstVertice 
+	where 	noeuds = catMaybes . fmap (\(x,y)-> if (Var x) `elem` l 
+			then Just (Var x,y)
+			else Nothing )
+			$ fmap swap dic
+		dic = labNodes g
+
+-----Case outputpersistency -----
+
+
+satifyF g iso clause n =
+	catMaybes . fmap (removeWrong n)$ foldl (\acc i->(++) acc . catMaybes $ fmap 
+					(\(IClause a b) -> 
+							case satifyCP g i b of
+								Nothing -> Nothing
+								Just l ->  Just $ IClause (fromMaybe [] $ satifyCN g i a) (l))
+					 $ clause )
+		[]	
+		iso 
+
+removeWrong n clause = case clause of --this will do nothing for the last output persistency 
+	IClause ((Atom _ (Var s1:Var s1':_)):(Atom _ (Var s2:Var s2':_)):[]) (_)  ->  
+			let	p1 = event s1 s1' in		
+			let	p2 = event s2 s2' in	
+			if (p1>=n || p2>= n )
+					then Just clause
+					else Nothing 
+	_ -> Just clause
+
+event a b = fromJust . findIndex (\(x,y)-> x /=y )$ a `zip` b
+  
+satifyCP g i l =simplify g. fmap (substitue g i)$ l
+
+satifyCN g i l= simplify g. fmap (substitue g i) $l
+
+substitue g i (Atom s l) = --adaptation for persistency for this existing quantifier
+	Atom s ((fmap (\x -> if x == FOL.Const "Skol8" [Var "x4",Var "x2", Var "x1"] then existRemover g (i Map.! (Var "x1")) (i Map.! (Var "x2")) (i Map.! (Var "x4")) else i Map.! x) $ take 2 l)++drop 2 l) 
+
+existRemover g s1 s2 s3 =
+	case s3 of
+		Var s -> Var $ take (transition ) s ++ (\x->if x=='0' then "1" else "0") (s!!transition)  ++ drop (transition+1) s   
+		_ -> undefined
+	where 	transition =   fromJust . findIndex (\(x,y)-> x /=y ) $ (show s1) `zip` (show s2) 
+
+
+
+--- Formulas in the model 
+
+printSatFormulas [] stateGraph n = []
+printSatFormulas normalized@(t:q) stateGraph n = 
+	satifyF stateGraph (concat $ allIsomorphisms normalized stateGraph) normalized n
+	++ ( printSatFormulas q stateGraph n )
+
+
+outputPersistency2 :: Formula Input
+outputPersistency2 = forall $ \sommet -> 
+	(forall $ \voisin1 -> (forall $ \voisin2 ->
+	atom "E" [sommet,voisin1] `impl`
+	(atom "E" [sommet,voisin2] `impl`
+	((FOL.not $ atom "Eq" [voisin1,voisin2]) `impl`
+	((exists $ \completeDiagram -> 
+	atom "E" [voisin1,completeDiagram] `FOL.and`
+	atom "E" [voisin2,completeDiagram]))))))
+
+
+inputCannotInput :: Formula Input 
+inputCannotInput = forall $ \sommet -> 
+	(forall $ \voisin1 -> (forall $ \voisin2 ->
+	atom "E" [sommet,voisin1] `impl`
+	(atom "E" [voisin1, voisin2] `impl`
+	((forall $ \completeDiagram -> 
+	atom "E" [sommet,completeDiagram] `FOL.and`
+	atom "E" [completeDiagram,voisin2])))))
+
+
+living g =  normalize . foldl (\acc noeud ->acc `FOL.and` 
+				(bigOrPre g noeud  `impl` bigOrSuc g noeud ) `FOL.and`
+				(bigOrSuc g noeud  `impl` bigOrPre g noeud )) tt $ nodes g   
+
+
+defineReachable g = normalize . foldl (\acc noeud ->acc `FOL.and` 
+				(atom "S" [Var . fromJust. lab g $ noeud]  `impl` bigOrPre g noeud ) `FOL.and`
+				(bigOrPre g noeud  `impl` atom "S" [Var . fromJust . lab g$ noeud ])) tt $ nodes g   
+
+
+propagateSignals g nbInputs = normalize . foldl (\acc noeud ->acc `FOL.and` 
+				(atom "S" [Var . fromJust. lab g $ noeud]  `impl` bigAndSucOut g noeud nbInputs ) 
+				) tt $ nodes g   
+
+
+--Actually covered by the input implies input
+mutually g =normalize . foldl (\ acc x -> foldl (\acc y -> if x `elem` pre g y  
+				then acc `FOL.and` mutuallyExclusive (fromJust .lab g $ x) (fromJust . lab g $ y)
+				else acc) acc $ pre g x) tt $ nodes g  
+
+
+mutuallyExclusive s1 s2 = (FOL.not (e1 `FOL.and` e2))
+		where 	e1 = atom "E" [ Var $ s1, Var $ s2 ] 
+			e2 = atom "E" [ Var $ s2, Var $ s1 ] 
+
+
+--For Minisat+
+variablesSat :: Gr String String -> String
+variablesSat sg =  ("Maximize\n\t " ++) . intercalate " + " . fmap (\(x,y)->
+					(\(a,b)->"E"++a++b).(\(a,b)->(fromJust a, fromJust b)) $(lab sg x ,lab sg y))
+				$ Data.Graph.Inductive.Graph.edges sg
+
+
+--Tools
+
+intF [] = 0
+intF (t:q) = if t then 2* intF q +1 else 2 * intF q 
+
+bigOrPre g noeud = foldl (\acc t -> atom "E" [Var .fromJust. lab g $ t, Var . fromJust . lab g $ noeud] `FOL.or` acc) ff $ pre g noeud
+
+bigOrSuc g noeud = foldl (\acc t -> atom "E" [Var .fromJust. lab g $ noeud, Var . fromJust . lab g $ t] `FOL.or` acc) ff $ suc g noeud
+
+bigAndSucOut g noeud n = foldl (\acc t -> --Off by one error possible todo
+				if (>= n) . fromJust . findIndex (\(x,y)-> x /=y )$ (fromJust . lab g $ t) `zip` (fromJust.lab g $ noeud) 
+					then atom "E" [Var .fromJust. lab g $ noeud, Var  . fromJust . lab g $ t] `FOL.and` acc
+					else acc) tt $ suc g noeud
+
+
+
+
+
+--USELESS but not necessary dead (for example extra silent formulas in the model)
 giveName x = case x of
 		Atom s l -> fmap (\m -> case m of Var s -> s) $ take 2 l 
 
@@ -235,6 +317,7 @@ addVertex c (t:q) g = (\x-> if x==[] then addVertex c q g else x) $
 					 addVertex c q (insEdge (vertexOf ng $ a, vertexOf ng $ b,"") $ ng)
 				else addVertex c q g
 
+
 outputPersistency :: Formula Input
 outputPersistency = forall $ \sommet -> 
 	(forall $ \voisin1 -> (forall $ \voisin2 ->
@@ -244,58 +327,5 @@ outputPersistency = forall $ \sommet ->
 	((forall $ \completeDiagram -> 
 	atom "E" [voisin1,completeDiagram] `impl`
 	atom "E" [voisin2,completeDiagram]))))))
-
-
-outputPersistency2 :: Formula Input
-outputPersistency2 = forall $ \sommet -> 
-	(forall $ \voisin1 -> (forall $ \voisin2 ->
-	atom "E" [sommet,voisin1] `impl`
-	(atom "E" [sommet,voisin2] `impl`
-	((FOL.not $ atom "Eq" [voisin1,voisin2]) `impl`
-	((exists $ \completeDiagram -> 
-	atom "E" [voisin1,completeDiagram] `FOL.and`
-	atom "E" [voisin2,completeDiagram]))))))
-
-
--- Two constants propreties, easy to express without this logic stuff
-
-bigOrPre g noeud = foldl (\acc t -> atom "E" [Var .fromJust. lab g $ t, Var . fromJust . lab g $ noeud] `FOL.or` acc) ff $ pre g noeud
-bigOrSuc g noeud = foldl (\acc t -> atom "E" [Var .fromJust. lab g $ noeud, Var . fromJust . lab g $ t] `FOL.or` acc) ff $ suc g noeud
-
-living g =  normalize . foldl (\acc noeud ->acc `FOL.and` 
-				(bigOrPre g noeud  `impl` bigOrSuc g noeud ) `FOL.and`
-				(bigOrSuc g noeud  `impl` bigOrPre g noeud )) tt $ nodes g   
-
-
-defineReachable g = normalize . foldl (\acc noeud ->acc `FOL.and` 
-				(atom "S" [Var . fromJust. lab g $ noeud]  `impl` bigOrPre g noeud ) `FOL.and`
-				(bigOrPre g noeud  `impl` atom "S" [Var . fromJust . lab g$ noeud ])) tt $ nodes g   
-
-bigAndSucOut g noeud n = foldl (\acc t -> --Off by one error possible todo
-				if (>= n) . fromJust . findIndex (\(x,y)-> x /=y )$ (fromJust . lab g $ t) `zip` (fromJust.lab g $ noeud) 
-					then atom "E" [Var .fromJust. lab g $ noeud, Var  . fromJust . lab g $ t] `FOL.and` acc
-					else acc) tt $ suc g noeud
-
-
-propagateSignals g nbInputs = normalize . foldl (\acc noeud ->acc `FOL.and` 
-				(atom "S" [Var . fromJust. lab g $ noeud]  `impl` bigAndSucOut g noeud nbInputs ) 
-				) tt $ nodes g   
-
-
-mutuallyExclusive s1 s2 = (FOL.not (e1 `FOL.and` e2))
-		where 	e1 = atom "E" [ Var $ s1, Var $ s2 ] 
-			e2 = atom "E" [ Var $ s2, Var $ s1 ] 
-
---A little bit too much but who cares
-mutually g =normalize . foldl (\ acc x -> foldl (\acc y -> if x `elem` pre g y  
-				then acc `FOL.and` mutuallyExclusive (fromJust .lab g $ x) (fromJust . lab g $ y)
-				else acc) acc $ pre g x) tt $ nodes g  
-
---For Minisat+
-variablesSat :: Gr String String -> String
-variablesSat sg =  ("Maximize\n\t " ++) . intercalate " + " . fmap (\(x,y)->
-					(\(a,b)->"E"++a++b).(\(a,b)->(fromJust a, fromJust b)) $(lab sg x ,lab sg y))
-				$ Data.Graph.Inductive.Graph.edges sg
-
 
 
