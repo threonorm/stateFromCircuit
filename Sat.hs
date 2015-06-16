7003a5027e5a316192c8657342a7360fadd78791
Thomas Emile Bourgeat <thomas.bourgeat@ens.fr>
2015-06-16 17:17:12 +0200

    sat

--- Sat.hs
+++ Sat.hs
@@ -51,7 +51,7 @@ main =do
 						--putStrLn . show . pretty . normalize $ atom "S" [Var . take (fromIntegral $n_v sg) $  repeat '0' ] 	
 						putStrLn . show . pretty . printSatFormulas (normalize outputPersistency2) csg nin $ inte 
 						putStrLn . show . pretty . printSatFormulas (normalize inputCannotInput) csg nin $ inte  
-						putStrLn . show . pretty . printSatFormulas (normalize forwardPersistency2) csg nin $ inte  
+						--putStrLn . show . pretty . printSatFormulas (normalize forwardPersistency2) csg nin $ inte  
 						putStrLn . show . pretty . living $ csg 
 						putStrLn . show . pretty . defineReachable $ csg 
 					      	putStrLn . show . pretty . propagateSignals csg . fromIntegral .n_inputs $ sg      
