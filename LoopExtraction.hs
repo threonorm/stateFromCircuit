d38d7f0e37e60ac519a2acfabb9c177867668c07
Thomas Emile Bourgeat <thomas.bourgeat@ens.fr>
2015-06-30 19:07:37 +0200

    Loop done

--- LoopExtraction.hs
+++ LoopExtraction.hs
@@ -30,22 +30,24 @@ import Data.Graph.Inductive.Graph
 import Data.Graph.Inductive.Example
 import Data.Graph.Inductive.PatriciaTree
 
+import Data.Text (pack)
 import System.Environment
 import Turtle
 
 nextNotUsed [] =  return ExitSuccess
 nextNotUsed (t:q) = do
-	_ <- shell (script ((!!1) lArgs) t) empty  
-	_ <- cp (((!!1) lArgs) ++ ".csg")  (((!!1) lArgs) ++ show t ++".csg")  
+	lArgs<- getArgs
+	_ <- shell (pack $ script ((!!1) lArgs) t) Turtle.empty  
+	_ <- cp (fromText . pack $ ((!!1) lArgs) ++ ".csg")  (fromText . pack $((!!1) lArgs) ++ show t ++".csg")  
 	myE<- parseFromFile solutionParser $  ((!!1) lArgs)++ ".solReadable" 
 	case myE of
-		Right myEdges -> mynextNotUsed ((t:q) \\ (fmap (\(x,y)-> event x y)  myEdges)) 
+		Right myEdges -> nextNotUsed ((t:q) \\ (fmap (\(x,y)-> event x y)  myEdges)) 
 		Left _ -> return $ ExitFailure 1  
 script v1 t =
 	"./StateFromCircuit/Sat "++ v1 ++" | sed \"s/;//\" > "++ v1 ++".lp\n"++ --add the line of 
 	"./gurobi.sh gurosolve.py " ++ v1 ++ 
-	"\ncat " ++ v1 ++ ".sol | grep -v \" 0\" | grep -v "#" | sed \"s/ 1//\" | sort > "++ v1 ++ ".solReadable"++ 
+	"\ncat " ++ v1 ++ ".sol | grep -v \" 0\" | grep -v \"#\" | sed \"s/ 1//\" | sort > "++ v1 ++ ".solReadable"++ 
 	"\n./StateFromCircuit/Printer " ++ v1 ++ " "++ v1 ++".solReadable > " ++ v1 ++ ".csg"
 
 
-main = nextNotUser [0..2]
+main = nextNotUsed [0..2]
